f67941fa820cdddd0dea7234385bee05622df874
oleh-hrebchuk <oleh.hrebchuk@gmail.com>
2016-09-20 18:06:44 +0300

    fix pattern

--- nmap_scaner.py
+++ nmap_scaner.py
@@ -9,7 +9,7 @@ from sys import argv
 total = len(argv)
 
 try:
-    dir = os.chdir(str(argv[1]))
+    dir = str(argv[1])
     port = str(argv[2])
     host = str(argv[3])
     serviname = str(argv[4])
@@ -23,28 +23,30 @@ except:
     namelog = "log.txt"
 
 if total < 6 or total > 6:
-        print ("Params: dir_log, port_scann, host_scann, service_name, name_log")
-#3389/tcp open  ms-wbt-server
-#port           servicename
-nmap_params = "nmap -sT -Pn -n -p {} {}".format(port,host)
-pattern = re.compile(r"^{}/tcp.(\w*).{}$".format(port,serviname))
+    print ("Params: dir_log, port_scann, host_scann, service_name, name_log")
+
+nmap_params = "nmap -sT -Pn -n -p {} {}".format(port, host)
+pattern = re.compile(r"^{}/tcp.(\w+).?{}$".format(port, serviname))
 get_status = subprocess.check_output("{}".format(nmap_params),
                                      stderr=subprocess.STDOUT,
                                      shell=True
                                      )
 
-def write_log(val):
 
-    with open('{}'.format(namelog), 'w')as f_log:
-       f_log.write(val+'\n')
+def write_log(val):
+    with open('{}{}'.format(dir,namelog), 'w')as f_log:
+        f_log.write(val + '\n')
+
 
 def main():
-
     list_com = [line for line in get_status.split('\n') if pattern.findall(line)]
     if 'open' in list_com[0]:
         write_log('1')
+        print('1')
     else:
         write_log('0')
+        print('0')
+
 
 if __name__ == "__main__":
     main()
