3b488d8994aa4d7ce1201b99e6aed1542b1ee113
oleh-hrebchuk <oleh.hrebchuk@gmail.com>
2016-09-27 15:30:57 +0300

    del echo

--- auth-via-file.sh
+++ auth-via-file.sh
@@ -2,20 +2,14 @@
 res_pass=`echo -n $2 | md5sum | awk '{print $1}'`
 echo $res_pass
 dir="$(dirname "$0")"
-echo $dir
 users=$(cat "$dir"/user.pass)
 
 for i in $users
-   do
-
-   if [ "$i" = "$1:$res_pass" ]
+do
+if [ "$i" = "$1:$res_pass" ]
    	then
-echo "0"
-
-		exit 0
-   else
-echo "1"
-
-		exit 1
+	    exit 0
+	else
+	    exit 1
 fi
 done
